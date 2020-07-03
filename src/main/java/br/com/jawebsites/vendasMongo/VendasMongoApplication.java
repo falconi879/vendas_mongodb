d3f515791379f267a7c4706e06d22c7e0939cef8
Andre Relhas <falconi879@gmail.com>
2020-07-03 11:50:57 -0300

    projeto criado

---
+++ src/main/java/br/com/jawebsites/vendasMongo/VendasMongoApplication.java
@@ -0,0 +1,13 @@
+package br.com.jawebsites.vendasMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jawebsites/vendasMongo/VendasMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jawebsites.vendasMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
