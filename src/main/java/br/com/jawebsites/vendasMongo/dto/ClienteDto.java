87afb28acc2b7b87877031860c139f6487ee5149
Andre Relhas <falconi879@gmail.com>
2020-07-04 10:24:02 -0300

    Dto retornando nome, id, usuario do Cliente

---
+++ src/main/java/br/com/jawebsites/vendasMongo/dto/ClienteDto.java
@@ -0,0 +1,41 @@
+package br.com.jawebsites.vendasMongo.dto;
+
+import java.io.Serializable;
+
+import br.com.jawebsites.vendasMongo.domain.Cliente;
+
+public class ClienteDto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String id; 
+	private String nome;
+	private String usuario;
+		
+	public ClienteDto() {
+	}
+	public ClienteDto(Cliente cliente) {
+		id = cliente.getId();
+		nome = cliente.getNome();
+		usuario = cliente.getUsuario();
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	
+
+}
--- src/main/java/br/com/jawebsites/vendasMongo/resources/ClienteResurce.java
+++ src/main/java/br/com/jawebsites/vendasMongo/resources/ClienteResurce.java
@@ -1,6 +1,7 @@
 package br.com.jawebsites.vendasMongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.jawebsites.vendasMongo.domain.Cliente;
+import br.com.jawebsites.vendasMongo.dto.ClienteDto;
 import br.com.jawebsites.vendasMongo.service.ClienteService;
 
 @RestController
@@ -18,10 +20,11 @@ public class ClienteResurce {
 	@Autowired
 	private ClienteService servico;
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<Cliente>> findAll() {		
+ 	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDto>> findAll() {		
 		List<Cliente> list = servico.findAll();
+		List<ClienteDto> listDto = list.stream().map(x -> new ClienteDto(x)).collect(Collectors.toList());
 		
-		return ResponseEntity.ok().body(list); 
+		return ResponseEntity.ok().body(listDto); 
 	}
 }
